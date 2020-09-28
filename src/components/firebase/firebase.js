2f56a0d9432baaef68f6b35a04f8d34c579e28cc
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-09-27 21:21:17 -0700

    Fixed env variables bug

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	databaseURL: process.env.databaseURL,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId,
-	measurementId: process.env.measurementId
+	apiKey:process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASEURL,
+	projectId: process.env.REACT_APP_PROJECTID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_APPID,
+	measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 class Firebase {
@@ -21,16 +21,13 @@ class Firebase {
 
 	doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
 
-  doSignInWithEmailAndPassword = (email, password) =>
-  this.auth.signInWithEmailAndPassword(email, password);
+	doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
 
-  doSignOut = () => this.auth.signOut();
+	doSignOut = () => this.auth.signOut();
 
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
- 
-  doPasswordUpdate = password =>
-    this.auth.currentUser.updatePassword(password);
+	doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
 
+	doPasswordUpdate = (password) => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
