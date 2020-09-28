3f608da4bd6ee1ba6e5a9e386071867a2ddca0e9
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-09-27 20:38:01 -0700

    Started firebase authentication

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,10 +1,36 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
-    apiKey: process.env.apiKey,
-    authDomain:process.env.authDomain,
-    databaseURL:process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-  };
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	databaseURL: process.env.databaseURL,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
+	measurementId: process.env.measurementId
+};
+
+class Firebase {
+	constructor() {
+		app.initializeApp(config);
+		this.auth = app.auth();
+	}
+	// *** Auth API ***
+
+	doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+  this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+ 
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+
+}
+
+export default Firebase;
